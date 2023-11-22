d004dacb3a723af062a6300a4a0bbeeda90069d5
Israelshecktar <iamshecktar1996@.com>
2023-11-22 15:04:39 +0100

    i was bored so i built a simple loan app calculator! enjoy

---
+++ loan_app/README.md
@@ -0,0 +1,74 @@
+# Loan Calculator App
+
+## Description
+
+Shecktar Loan Calculator App is a simple and user-friendly app that helps you estimate your monthly payments, interest rate, and loan term based on the loan amount and other factors. You can also choose to borrow the loan alone or split it with some friends. The app will calculate the payment per person and the payment due date for you. You can use this app to plan your budget and compare different loan options.
+
+## Features
+
+- Choose between dollars or naira as the currency
+- Choose between monthly or yearly interest as the payment option
+- Choose between now or later as the borrow date
+- Enter the loan amount, the number of people to split the loan with, and the loan duration
+- Get the total payment, the monthly payment, the payment per person, and the payment due date
+- Validate the input and handle errors
+- Format the output with commas and currency symbols
+
+## Installation
+
+To install this app, you need to have Python 3 installed on your computer. You can download Python 3 from [here]. You also need to have the datetime module, which is a built-in module in Python. You can check if you have the datetime module by running the following command in your terminal:
+
+```python
+python -c "import datetime"
+```
+
+If you don't get any error message, you have the datetime module installed. If you get an error message, you need to install the datetime module by running the following command in your terminal:
+
+```python
+pip install datetime
+```
+
+After you have Python 3 and the datetime module installed, you can download the loan_calculator.py file from this repository and save it in your preferred location. You can also clone this repository by running the following command in your terminal:
+
+```python
+git clone https://github.com/name/loan_calculator_app.git
+```
+
+## Usage
+
+To use this app, you need to run the loan_calculator.py file in your terminal. You can do this by navigating to the directory where you saved the file and running the following command:
+
+```python
+python loan_calculator.py
+```
+
+The app will greet you and ask for your name. Then, it will ask you a series of questions about your loan preferences. You need to enter your answers according to the instructions given by the app. For example, you need to enter 1 or 2 for some questions, and enter a valid number or date for other questions. The app will validate your input and handle any errors. After you enter all the required information, the app will calculate and display the results for you. It will also ask you if that would be all for now. You can enter 1 for yes or 2 for no. If you enter 1, the app will thank you and exit. If you enter 2, the app will ask you what else you would like to do or otherwise transfer you to a customer care representative.
+
+Here is an example of how the app works:
+
+```
+Welcome to shecktar loan calculator app!
+What is your name? John
+Hello, John!
+Would you like to borrow in dollars or naira? (1 for dollars, 2 for naira) 1
+How much would you like to borrow in $? 10000
+Would you like to borrow the loan alone or split the loan with some friends? (1 for alone, 2 for friends) 2
+How many people to split the loan bill with? 3
+Would you like to pay monthly interest or yearly interest? (1 for monthly, 2 for yearly) 1
+How long would you like to borrow the loan for in months? 12
+The total payment for the loan is $10,700.00
+The monthly payment for the loan is $891.67
+The payment per person for the loan is $3,566.67
+What date would you like to borrow the loan? (1 for now, 2 for later) 1
+The payment due date is 22-11-2024
+Would that be all for now? (1 for yes, 2 for no) 1
+Thank you for using my loan calculator app, John! Have a nice day!
+```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE] file for details.
+
+## Contributors
+
+This project was created with love by shecktar.
---
+++ loan_app/loan_calculator.py
@@ -0,0 +1,162 @@
+# print a statement welcome the user to my loan calculator app
+print("Welcome to shecktar loan calculator app!")
+
+# Ask for the users name and greet the user
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+# Ask if the user will like to borrow in dollars or naira
+currency_option = input("Would you like to borrow in dollars or naira? (1 for dollars, 2 for naira) ")
+
+# validate the input
+while currency_option not in ["1", "2"]:
+    print("Please enter a valid response.")
+    currency_option = input("Would you like to borrow in dollars or naira? (1 for dollars, 2 for naira) ")
+
+# set the currency symbol
+if currency_option == "1":
+    currency_symbol = "$"
+else:
+    currency_symbol = "₦"
+
+# Ask the user how much they would like to borrow
+loan_amount = input(f"How much would you like to borrow in {currency_symbol}? ")
+
+# validate the input
+while not loan_amount.replace(",", "").isnumeric():
+    print("Invalid amount. Please enter a valid amount.")
+    loan_amount = input(f"How much would you like to borrow in {currency_symbol}? ")
+
+# convert the input to a float and remove the commas
+loan_amount = float(loan_amount.replace(",", ""))
+
+# Ask if the user would like to borrow the loan alone or split the loan with some friends
+split_option = input("Would you like to borrow the loan alone or split the loan with some friends? (1 for alone, 2 for friends) ")
+
+# validate the input
+while split_option not in ["1", "2"]:
+    print("Please enter a valid response.")
+    split_option = input("Would you like to borrow the loan alone or split the loan with some friends? (1 for alone, 2 for friends) ")
+
+# Ask the user how many people to split the loan bill with
+if split_option == "2":
+    num_friends = input("How many people to split the loan bill with? ")
+    # validate the input
+    while not num_friends.isnumeric():
+        print("Invalid number. Please enter a valid number.")
+        num_friends = input("How many people to split the loan bill with? ")
+    # convert the input to an int
+    num_friends = int(num_friends)
+else:
+    num_friends = 1
+
+# Ask the user to choose between monthly interest or yearly interest
+interest_option = input("Would you like to pay monthly interest or yearly interest? (1 for monthly, 2 for yearly) ")
+
+# validate the input
+while interest_option not in ["1", "2"]:
+    print("Please enter a valid response.")
+    interest_option = input("Would you like to pay monthly interest or yearly interest? (1 for monthly, 2 for yearly) ")
+
+# Ask the user for the duration they'd like to borrow the loan for
+loan_duration = input("How long would you like to borrow the loan for in months? ")
+
+# validate the input
+while not loan_duration.isnumeric():
+    print("Invalid number. Please enter a valid number.")
+    loan_duration = input("How long would you like to borrow the loan for in months? ")
+
+# convert the input to an int
+loan_duration = int(loan_duration)
+
+# for monthly interest it is 7% interest rate
+if interest_option == "1":
+    interest_rate = 0.07
+# for yearly interest it is 85% interest rate
+else:
+    interest_rate = 0.85
+
+# calculate the total due payment for the duration
+total_payment = loan_amount * (1 + interest_rate * loan_duration / 12)
+
+# calculate the monthly payment
+monthly_payment = total_payment / loan_duration
+
+# calculate the payment per person
+payment_per_person = total_payment / num_friends
+
+# define a function to format the money with commas
+def format_money(amount):
+    # convert the amount to a string with two decimal places
+    amount = f"{amount:.2f}"
+    # split the amount into the integer and fractional parts
+    integer, fraction = amount.split(".")
+    # reverse the integer part
+    integer = integer[::-1]
+    # insert a comma after every three digits
+    integer = ",".join([integer[i:i+3] for i in range(0, len(integer), 3)])
+    # reverse the integer part back
+    integer = integer[::-1]
+    # return the formatted amount
+    return integer + "." + fraction
+
+# print the results
+print(f"The total payment for the loan is {currency_symbol}{format_money(total_payment)}")
+print(f"The monthly payment for the loan is {currency_symbol}{format_money(monthly_payment)}")
+if num_friends > 1:
+    print(f"The payment per person for the loan is {currency_symbol}{format_money(payment_per_person)}")
+
+# Ask the user what date they would like to borrow the loan
+borrow_date = input("What date would you like to borrow the loan? (1 for now, 2 for later) ")
+
+# validate the input
+while borrow_date not in ["1", "2"]:
+    print("Please enter a valid response.")
+    borrow_date = input("What date would you like to borrow the loan? (1 for now, 2 for later) ")
+
+# import the datetime module
+import datetime
+
+# get the current date
+today = datetime.date.today()
+
+# if now, return the payment due date
+if borrow_date == "1":
+    # calculate the payment due date by adding the loan duration to the current date
+    due_date = today + datetime.timedelta(days=loan_duration * 30)
+    # change the payment due date format to dd-mm-yyyy
+    due_date = due_date.strftime("%d-%m-%Y")
+    print(f"The payment due date is {due_date}")
+# if later, calculate the payment due date
+else:
+    # ask the user for the date they want to borrow the loan
+    borrow_date = input("Please enter the date you want to borrow the loan in dd-mm-yyyy format: ")
+    # validate the input
+    while True:
+        try:
+            # convert the input to a date object
+            borrow_date = datetime.datetime.strptime(borrow_date, "%d-%m-%Y").date()
+            break
+        except ValueError:
+            print("Invalid date. Please enter a valid date.")
+            borrow_date = input("Please enter the date you want to borrow the loan in dd-mm-yyyy format: ")
+    # calculate the payment due date by adding the loan duration to the borrow date
+    due_date = borrow_date + datetime.timedelta(days=loan_duration * 30)
+    # change the payment due date format to dd-mm-yyyy
+    due_date = due_date.strftime("%d-%m-%Y")
+    print(f"The payment due date is {due_date}")
+
+# Ask the user if that would be all for now
+answer = input("Would that be all for now? (1 for yes, 2 for no) ")
+
+# validate the input
+while answer not in ["1", "2"]:
+    print("Please enter a valid response.")
+    answer = input("Would that be all for now? (1 for yes, 2 for no) ")
+
+# if yes, thank the user and exit the app
+if answer == "1":
+    print(f"Thank you for using my loan calculator app, {name}! Have a nice day!")
+# if no, ask the user what they would like to do or otherwise transfer them to a customer care representative
+else:
+    print(f"What else would you like to do, {name}? If you need more help, please contact our customer care representative at 0903-377-3871.")
